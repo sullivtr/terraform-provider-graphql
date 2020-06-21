460b51f5f222177e9e04b98c92ff4eae29fbb776
tyler.sullivan <tyler.sullivan@tylertech.com>
2020-06-20 22:09:17 -0700

    feat: calculate delete mutation variables

--- test/test_basic/test.tf
+++ test/test_basic/test.tf
@@ -8,7 +8,7 @@ provider "graphql" {
 data "graphql_query" "basic_query" {
   depends_on = [graphql_mutation.basic_mutation]
   read_query_variables = {}
-  read_query     = "${file("./queries/readQuery")}"
+  read_query     = file("./queries/readQuery")
 }
 
 resource "graphql_mutation" "basic_mutation" {
@@ -16,18 +16,16 @@ resource "graphql_mutation" "basic_mutation" {
     "text" = "Here is something todo"
     "userId" = "98"
   }
-
   # if update, create, and read variables are omitted, they will fall back to the required create_mutation_variables
   update_mutation_variables = {}
-  delete_mutation_variables = {
-    "id" = "T5577006791947779410"
-  }
   read_query_variables = {}
   # Reference files instead of inline queries to keep tf files clean. See examplquery for an example of a query file
-  create_mutation = "${file("./queries/createMutation")}"
-  update_mutation = "${file("./queries/updateMutation")}"
-  delete_mutation = "${file("./queries/deleteMutation")}"
-  read_query      = "${file("./queries/readQuery")}"
+  create_mutation = file("./queries/createMutation")
+  update_mutation = file("./queries/updateMutation")
+  delete_mutation = file("./queries/deleteMutation")
+  read_query      = file("./queries/readQuery")
+
+  query_response_key_map = ["todo", "id"]
 }
 
 output "myoutput" {
