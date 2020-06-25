de3522e25c360326d391b69b9090b0ef7ab5c543
tyler.sullivan <tyler.sullivan@tylertech.com>
2020-06-25 13:39:30 -0700

    test: remove old comments from test.tf

--- test/test_basic/test.tf
+++ test/test_basic/test.tf
@@ -5,20 +5,12 @@ provider "graphql" {
   }
 }
 
-# data "graphql_query" "basic_query" {
-#   depends_on = [graphql_mutation.basic_mutation]
-#   read_query_variables = {}
-#   read_query     = file("./queries/readQuery")
-# }
-
 resource "graphql_mutation" "basic_mutation" {
   mutation_variables = {
     "text" = var.todo_text
     "userId" = var.todo_user_id
   }
-  # if update, create, and read variables are omitted, they will fall back to the required create_mutation_variables
   read_query_variables = {}
-  # Reference files instead of inline queries to keep tf files clean. See examplquery for an example of a query file
   create_mutation = file("./queries/createMutation")
   update_mutation = file("./queries/updateMutation")
   delete_mutation = file("./queries/deleteMutation")
