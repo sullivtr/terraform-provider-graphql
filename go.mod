a8e1139da7dfdd7d929ddc5b796c788bb12811dd
Patrick Decat <pdecat@gmail.com>
2021-12-24 11:17:42 +0100

    Use existing response key/value parsers instead of HCL parser

--- go.mod
+++ go.mod
@@ -6,13 +6,11 @@ require (
 	cloud.google.com/go/storage v1.12.0 // indirect
 	github.com/aws/aws-sdk-go v1.34.33 // indirect
 	github.com/fatih/color v1.9.0 // indirect
-	github.com/hashicorp/hcl/v2 v2.3.0
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.7.0
 	github.com/jarcoal/httpmock v1.0.6
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
 	github.com/stretchr/testify v1.7.0
-	github.com/zclconf/go-cty v1.8.4
 	golang.org/x/tools v0.0.0-20200929173036-5272f303b6eb // indirect
 	google.golang.org/genproto v0.0.0-20200929141702-51c3e5b607fe // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
--- graphql/provider.go
+++ graphql/provider.go
@@ -2,16 +2,11 @@ package graphql
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/zclconf/go-cty/cty"
-	"github.com/zclconf/go-cty/cty/function"
-	"github.com/zclconf/go-cty/cty/function/stdlib"
-	"github.com/zclconf/go-cty/cty/gocty"
 )
 
 func Provider() *schema.Provider {
@@ -73,34 +68,18 @@ func graphqlConfigure(ctx context.Context, d *schema.ResourceData) (interface{},
 			return nil, *queryErrors
 		}
 
-		var queryResponseCty cty.Value
-		if queryResponseCty, err = gocty.ToCtyValue(string(resBytes), cty.String); err != nil {
+		var queryResponseData map[string]interface{}
+		if err := json.Unmarshal(resBytes, &queryResponseData); err != nil {
 			return nil, diag.FromErr(err)
 		}
 
-		evalCtx := &hcl.EvalContext{
-			Variables: map[string]cty.Value{
-				"oauth2_login_query_response": queryResponseCty,
-			},
-			Functions: map[string]function.Function{
-				"jsondecode": stdlib.JSONDecodeFunc,
-			},
-		}
-
-		var expr hcl.Expression
-		var hclDiags hcl.Diagnostics
-		valueTemplate := fmt.Sprintf("${jsondecode(oauth2_login_query_response).%s}", oauth2LoginQueryValueAttribute.(string))
-		if expr, hclDiags = hclsyntax.ParseTemplate([]byte(valueTemplate), "", hcl.InitialPos); len(hclDiags) > 0 {
-			return nil, convertDiagnosticsFromHCLToTerraformSDK(hclDiags)
-		}
-
-		var interpolatedValue cty.Value
-		if interpolatedValue, hclDiags = expr.Value(evalCtx); len(hclDiags) > 0 {
-			return nil, convertDiagnosticsFromHCLToTerraformSDK(hclDiags)
+		var value string
+		if value, err = getOAuth2LoginQueryAttributeValue(oauth2LoginQueryValueAttribute.(string), queryResponseData); err != nil {
+			return nil, diag.FromErr(err)
 		}
 
 		config.RequestAuthorizationHeaders = map[string]interface{}{
-			"Authorization": fmt.Sprintf("Bearer %s", interpolatedValue.AsString()),
+			"Authorization": fmt.Sprintf("Bearer %s", value),
 		}
 	}
 
@@ -114,14 +93,11 @@ type graphqlProviderConfig struct {
 	RequestAuthorizationHeaders map[string]interface{}
 }
 
-func convertDiagnosticsFromHCLToTerraformSDK(hclDiags hcl.Diagnostics) diag.Diagnostics {
-	diags := make(diag.Diagnostics, len(hclDiags))
-	for i, hclDiag := range hclDiags {
-		// HCL severity enum is: 0 (invalid), 1 (error), 2 (warning)
-		// terraform SDK severity enum is: 0 (error), 1 (warning)
-		diags[i].Severity = diag.Severity(hclDiag.Severity - 1)
-		diags[i].Summary = hclDiag.Summary
-		diags[i].Detail = hclDiag.Detail
+func getOAuth2LoginQueryAttributeValue(attribute string, data map[string]interface{}) (string, error) {
+	resourceKeyArgs := buildResourceKeyArgs(attribute)
+	value, err := getResourceKey(data, resourceKeyArgs...)
+	if err != nil {
+		return "", err
 	}
-	return diags
+	return value.(string), nil
 }
