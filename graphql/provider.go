ff9ea575706f6447581264d843cc275044eb709c
Patrick Decat <pdecat@gmail.com>
2021-12-24 11:42:14 +0100

    Error if some but not all three OAuth 2.0 attributes are set: `oauth2_login_query`, `oauth2_login_query_variables` and `oauth2_login_query_value_attribute`

--- graphql/provider.go
+++ graphql/provider.go
@@ -53,12 +53,18 @@ func Provider() *schema.Provider {
 }
 
 func graphqlConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	diags := diag.Diagnostics{}
+
 	config := &graphqlProviderConfig{
 		GQLServerUrl:   d.Get("url").(string),
 		RequestHeaders: d.Get("headers").(map[string]interface{}),
 	}
 
-	if oauth2LoginQueryValueAttribute := d.Get("oauth2_login_query_value_attribute"); d.Get("oauth2_login_query") != "" && oauth2LoginQueryValueAttribute != "" {
+	oauth2LoginQuery := d.Get("oauth2_login_query").(string)
+	oauth2LoginQueryVariables := d.Get("oauth2_login_query_variables").(map[string]interface{})
+	oauth2LoginQueryValueAttribute := d.Get("oauth2_login_query_value_attribute").(string)
+
+	if oauth2LoginQuery != "" && len(oauth2LoginQueryVariables) > 0 && oauth2LoginQueryValueAttribute != "" {
 		queryResponse, resBytes, err := queryExecute(ctx, d, config, "oauth2_login_query", "oauth2_login_query_variables")
 		if err != nil {
 			return nil, diag.FromErr(fmt.Errorf("unable to execute oauth2_login_query: %w", err))
@@ -74,16 +80,22 @@ func graphqlConfigure(ctx context.Context, d *schema.ResourceData) (interface{},
 		}
 
 		var value string
-		if value, err = getOAuth2LoginQueryAttributeValue(oauth2LoginQueryValueAttribute.(string), queryResponseData); err != nil {
+		if value, err = getOAuth2LoginQueryAttributeValue(oauth2LoginQueryValueAttribute, queryResponseData); err != nil {
 			return nil, diag.FromErr(err)
 		}
 
 		config.RequestAuthorizationHeaders = map[string]interface{}{
 			"Authorization": fmt.Sprintf("Bearer %s", value),
 		}
+	} else if oauth2LoginQuery != "" || len(oauth2LoginQueryVariables) > 0 || oauth2LoginQueryValueAttribute != "" {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Incomplete OAuth 2.0 provider configuration",
+			Detail:   "All three attributes must be set: `oauth2_login_query`, `oauth2_login_query_variables` and `oauth2_login_query_value_attribute`.",
+		})
 	}
 
-	return config, diag.Diagnostics{}
+	return config, diags
 }
 
 type graphqlProviderConfig struct {
